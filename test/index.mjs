48ee3973cde4d0da3d44f761ed501681b9d930c6
Luke Karrys <luke@lukekarrys.com>
2022-06-02 17:01:09 -0700

    chore: add tests (#10)
    * chore: add initial snapshot tests for help output
    
    * chore: fix postlint check
    
    * chore: fix snapshots
    
    * chore: run patch-package via tap node-arg
    
    * chore: convert path to file url before import
    
    * chore: fix path in snapshots

---
+++ test/index.mjs
@@ -0,0 +1,43 @@
+import t from 'tap'
+import { spawnSync } from 'child_process'
+import { dirname, resolve, sep } from 'path'
+import { fileURLToPath } from 'url'
+
+t.cleanSnapshot = (s) =>
+  s
+    .replace(/(--limit.*\[default: )(\d+)/g, '$1$NUM_CORES')
+    .split(process.env.HOME + sep)
+    .join('$HOME/')
+
+const root = resolve(dirname(fileURLToPath(import.meta.url)), '..')
+
+const getHelp = (...cmd) =>
+  spawnSync('node', ['./bin/gh.mjs', ...cmd, '--help'], {
+    encoding: 'utf-8',
+    cwd: root,
+  })
+
+t.test('help messages', async (t) => {
+  const commands = [
+    [],
+    ['clone'],
+    ['merge'],
+    ['merge', 'dependabot'],
+    ['merge', 'pending-release'],
+    ['merge', 'pull-requests'],
+    ['publish'],
+    ['template-oss-fix'],
+    ['dependabot'],
+    ['graphql'],
+    ['pending-release'],
+    ['pull-requests'],
+    ['repos'],
+  ]
+
+  for (const cmd of commands) {
+    const res = getHelp(...cmd)
+    t.equal(res.status, 0)
+    t.equal(res.stderr, '')
+    t.matchSnapshot(res.stdout, cmd.join(' '))
+  }
+})

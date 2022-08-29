2f2f405331627e70c35c29423e4c3ee3c1d24609
Luke Karrys <luke@lukekarrys.com>
2022-08-29 00:49:17 -0700

    feat: add new commands and queries (#23)
    BREAKING CHANGE: Query is now the first positional argument. Most
    commands can be run with the signature `<query> [worker]`. If the worker
    positional is not included, then the command will print the results of
    the query.

--- test/gh.mjs
+++ test/gh.mjs
@@ -1,7 +1,8 @@
 import t from 'tap'
 import { spawnSync } from 'child_process'
-import { dirname, resolve, sep } from 'path'
+import { dirname, resolve, sep, join } from 'path'
 import { fileURLToPath } from 'url'
+import * as utils from '../lib/gh/yargs/utils.mjs'
 
 t.cleanSnapshot = (s) =>
   s
@@ -17,27 +18,31 @@ const getHelp = (...cmd) =>
     cwd: root,
   })
 
-t.test('help messages', async (t) => {
+t.test('all commands', async (t) => {
+  const queries = await utils.readdirCommands(join(root, 'lib/gh/queries'))
+  const workers = await utils.readdirCommands(join(root, 'lib/gh/workers'))
+
   const commands = [
     [],
-    ['clone'],
-    ['merge'],
-    ['merge', 'dependabot'],
-    ['merge', 'pending-release'],
-    ['merge', 'pull-requests'],
-    ['publish'],
-    ['template-oss-fix'],
-    ['dependabot'],
-    ['graphql'],
-    ['pending-release'],
-    ['pull-requests'],
-    ['repos'],
+    ...queries.flatMap((query) => [
+      [query.name],
+      ...utils
+        .getWorkersByQuery(workers, query)
+        .map((worker) => [query.name, worker.name]),
+    ]),
   ]
 
+  t.matchSnapshot(
+    commands.map((c) => `gh ${c.join(' ')}`).join('\n'),
+    'all commands'
+  )
+
   for (const cmd of commands) {
-    const res = getHelp(...cmd)
-    t.equal(res.status, 0)
-    t.equal(res.stderr, '')
-    t.matchSnapshot(res.stdout, cmd.join(' '))
+    await t.test(`help ${cmd.join(' ')}`, async (t) => {
+      const res = getHelp(...cmd)
+      t.equal(res.status, 0)
+      t.equal(res.stderr, '')
+      t.matchSnapshot(res.stdout.trim())
+    })
   }
 })
